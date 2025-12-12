cedc9bd6485118f7b5e4274bcd05c9271e7d18f9
www1598g-gif <www1598g@gmail.com>
2025-12-12 18:27:02 +0800

    ok

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import { getDatabase } from "firebase/database";
 
 // 👇 下面這一串要換成你剛剛在 Firebase 網站上申請的喔！
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCYjJymGsrS_DWnndUR6MY2KUSmjXE-N7A",
   authDomain: "chiangmai-2026.firebaseapp.com",
@@ -13,7 +14,6 @@ const firebaseConfig = {
   messagingSenderId: "515846045286",
   appId: "1:515846045286:web:dec91f8868d5cdece5b7a7"
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
